e7ec46bd5480de3e1833b6aa559ef6a251e6136d
ktgreeds <kt.greeds@gmail.com>
2026-06-05 09:39:11 +0900

    Add files via upload

---
+++ JazeroPacketSwitch/JazeroPacketSwitch.lua
@@ -0,0 +1,17 @@
+_addon.name = 'JazeroPacketSwitch'
+_addon.author = 'ktgreeds'
+_addon.version = '1.0'
+
+local switch = true
+
+windower.register_event('addon command', function(...)
+    if switch then
+        windower.send_command('unload packetflow; unload jazero;')
+        windower.add_to_chat(167,windower.to_shift_jis('■■■ アンロード JazeroPacketSwitch ■■■'))
+    else
+        
+        windower.send_command('load packetflow; load jazero;')
+        windower.add_to_chat(167,windower.to_shift_jis('■■■ ロード JazeroPacketSwitch ■■■'))
+    end
+    switch = not switch  -- 'not'演算子でブール値を反転させる
+end)
